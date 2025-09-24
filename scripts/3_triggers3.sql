37973dd377ef4c61f07eebf5f8955e1794ad6b03
m4vi <vitlia40@gmail.com>
2025-09-24 18:49:16 -0400

    triggers 3

--- scripts/3_triggers3.sql
+++ scripts/3_triggers3.sql
@@ -1,10 +1,29 @@
-CREATE TABLE Log_Status_Voo (
-    id_log INT AUTO_INCREMENT PRIMARY KEY,
-    id_voo INT,
-    status_antigo VARCHAR(30),
-    status_novo VARCHAR(30),
-    data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+USE flyBase;
+DELIMITER //
+
+CREATE TRIGGER tg_StatusDoVoo
+AFTER UPDATE ON Voo
+FOR EACH ROW
+BEGIN
+
+    -- se o valor do status realmente mudou.
+    IF OLD.id_status_voo <> NEW.id_status_voo THEN
     
-    -- Chave estrangeira para garantir a integridade referencial
-    FOREIGN KEY (id_voo) REFERENCES Voo(id_voo)
-);
+        -- Variáveis para armazenar os nomes dos status (textos)
+        DECLARE nome_status_antigo VARCHAR(30);
+        DECLARE nome_status_novo VARCHAR(30);
+
+        -- Busca o nome do status antigo usando o ID antigo (OLD)
+        SELECT nome INTO nome_status_antigo FROM Status_voo WHERE id_status = OLD.id_status_voo;
+        
+        -- Busca o nome do status novo usando o ID novo (NEW)
+        SELECT nome INTO nome_status_novo FROM Status_voo WHERE id_status = NEW.id_status_voo;
+
+        -- Insere o registro completo na tabela de log.
+        INSERT INTO Log_Status_Voo (id_voo, status_antigo, status_novo)
+        VALUES (NEW.id_voo, nome_status_antigo, nome_status_novo);
+        
+    END IF;
+END //
+
+DELIMITER ;
