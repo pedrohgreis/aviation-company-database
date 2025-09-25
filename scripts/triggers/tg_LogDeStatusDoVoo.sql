f1421562867858cdd40b8b9779fec01fb7bc8db4
m4vi <vitlia40@gmail.com>
2025-09-24 20:31:46 -0400

    Update tg_LogDeStatusDoVoo.sql

--- scripts/triggers/tg_LogDeStatusDoVoo.sql
+++ scripts/triggers/tg_LogDeStatusDoVoo.sql
@@ -8,11 +8,9 @@ CREATE TRIGGER tg_LogDeStatusDoVoo
 AFTER UPDATE ON Voo
 FOR EACH ROW
 BEGIN
-    -- A condição principal continua a mesma.
+    
     IF OLD.id_status_voo <> NEW.id_status_voo THEN
     
-        -- CORREÇÃO: A lógica ficou muito mais simples e eficiente!
-        -- Não precisamos mais de DECLARE ou SELECT. Inserimos os IDs diretamente.
         INSERT INTO Log_Status_Voo (id_voo, id_status_antigo, id_status_novo)
         VALUES (NEW.id_voo, OLD.id_status_voo, NEW.id_status_voo);
         
